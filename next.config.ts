fefac15af073cf92ab0fd6628f97631a445c4853
timmyblive <tbutterfield@me.com>
2025-04-02 15:57:20 -0500

    Update next.config.ts for better deployment support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['prod.spline.design'],
+  },
+  webpack: (config) => {
+    // Add support for importing .splinecode files
+    config.module.rules.push({
+      test: /\.splinecode$/,
+      use: ['file-loader'],
+    });
+    
+    return config;
+  },
 };
 
 export default nextConfig;
